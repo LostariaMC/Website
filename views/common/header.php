66dc1475b3209fcb6561dc3c4f503247d8cf3cb0
ElloWorld <lara.dev01wnslf@gmail.com>
2022-09-04 22:30:26 +0200

    :sparkles: Add online players count

--- views/common/header.php
+++ views/common/header.php
@@ -50,7 +50,11 @@
             echo '<a class="navbar-brand" href="./">Lostaria</a>';
         }
         ?>
-
+        <p style="font-size: 1rem; color: white; padding-top: 1rem;">
+        <?php
+        echo "Joueurs connecté(s) : " . \utils\OnlinePlayers::getOnlinePlayers();
+        ?>
+        </p>
         <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex flex-grow justify-content-end flex-grow p-2">
             <a href="http://discord.lostaria.fr" target="_blank" class="btn btn-outline-light" style="margin-left: 10px;">Discord</a>
             <a href="https://github.com/LostariaMC" target="_blank" class="btn btn-outline-light" style="margin-left: 10px;">GitHub</a>
