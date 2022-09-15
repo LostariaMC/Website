908912d7b51068db5b37de0151eb2fd25902649d
Erpriex <erpriex@gmail.com>
2022-09-15 20:32:56 +0200

    🔍️ Add Jenkins btn (SEO)

--- views/common/header.php
+++ views/common/header.php
@@ -64,6 +64,7 @@
             <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex flex-grow justify-content-end flex-grow p-2">
                 <a href="http://discord.lostaria.fr" target="_blank" class="btn btn-outline-light" style="margin-left: 10px;">Discord</a>
                 <a href="https://github.com/LostariaMC" target="_blank" class="btn btn-outline-light" style="margin-left: 10px;">GitHub</a>
+                <a href="https://ci.lostaria.fr" target="_blank" class="btn btn-outline-light" style="margin-left: 10px;">Jenkins</a>
             </ul>
         </div>
     </nav>
