57dbf8e1ca55544caa94f4834215b7ae4786527a
Erpriex <erpriex@gmail.com>
2022-09-27 00:43:49 +0200

    🔥 Remove SEO patch HTTPS

--- index.php
+++ index.php
@@ -3,13 +3,6 @@
 use routes\base\Router;
 use utils\SessionHelpers;
 
-/*if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-    $location = 'https://lostaria.fr' . $_SERVER['REQUEST_URI']; // Domain : $_SERVER['HTTP_HOST']
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $location);
-    exit;
-}*/
-
 include("autoload.php");
 
 SessionHelpers::init();
