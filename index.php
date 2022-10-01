ae1851dbfd5309dc5c44319935913d387d3ad6c9
Erpriex <erpriex@gmail.com>
2022-10-01 20:26:40 +0200

    🔍️ Redo SEO

--- index.php
+++ index.php
@@ -5,6 +5,13 @@ use utils\SessionHelpers;
 
 include("autoload.php");
 
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $location = 'https://lostaria.fr' . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+}
+
 SessionHelpers::init();
 
 $router = new Router();
