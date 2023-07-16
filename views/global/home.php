d561138a729e51639d598a93700ad18e7c92a163
Erpriex <erpriex@gmail.com>
2023-07-16 21:16:31 +0200

    🔥 Remove Twitter embed

--- views/global/home.php
+++ views/global/home.php
@@ -1,27 +1,25 @@
-<div class="container" style="background-color: #FFF; border-radius: 10px;">
+<div class="container" style="background-color: #FFF; border-radius: 10px; padding-top: 15px; padding-left: 15px; padding-right: 55px;">
 
-    <div class="row">
-        <div style="padding-right: 50px;" class="col-md-8">
-            <h4 style="margin: 20px">Membres de l'équipe :</h4>
-            <?php
+    <h4 style="margin: 20px">Membres de l'équipe :</h4>
+    <?php
+    $i = 0;
+    foreach ($staffMembers as $p) {
+        if ($i >= 2) {
+            echo '</div>';
             $i = 0;
-            foreach ($staffMembers as $p) {
-                if ($i >= 2) {
-                    echo '</div>';
-                    $i = 0;
-                }
-                if ($i == 0) {
-                    echo '<div class="row">';
-                }
+        }
+        if ($i == 0) {
+            echo '<div class="row">';
+        }
 
-                $competences = "";
-                foreach ($p[1] as $c) {
-                    $competences = $competences . "<span style='margin-right: 3px' class=\"badge rounded-pill text-bg-primary text-light\">" . $c . "</span>";
-                }
+        $competences = "";
+        foreach ($p[1] as $c) {
+            $competences = $competences . "<span style='margin-right: 3px' class=\"badge rounded-pill text-bg-primary text-light\">" . $c . "</span>";
+        }
 
-                $staffName = \utils\MojangUtils::getName($p[0]);
+        $staffName = \utils\MojangUtils::getName($p[0]);
 
-                echo '
+        echo '
                 <div style="margin-left: 20px; margin-bottom: 20px;" class="card card-hover">
                         <div class="card-body d-flex">
                             <div class="p-3">
@@ -36,18 +34,12 @@
                         </div>
                     </div>
             ';
-                $i++;
-            }
-            if ($i >= 2) {
-                echo '</div>';
-            }
-            ?>
-            <!-- <a style="margin-top: 20px; margin-right: 30px; margin-bottom: 40px; float: right;" href="https://guide.lostaria.fr/team.html" target="_blank" class="btn btn-outline-primary">Qui sommes-nous →</a> -->
-        </div>
-        <div style="margin-top: 30px; padding-bottom: 40px;" class="col-md-4">
-            <a class="twitter-timeline" href="https://twitter.com/LostariaMC?ref_src=twsrc%5Etfw" data-height="900">Tweets par LostariaMC</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-    </div>
+        $i++;
+    }
+    if ($i >= 2) {
+        echo '</div>';
+    }
+    ?>
 </div>
 
 <style>
