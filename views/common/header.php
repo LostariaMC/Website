74ebf4b56922a553cfeeff9998112e0b9ee9e19d
Erpriex <erpriex@gmail.com>
2023-07-27 01:15:46 +0200

    ⚰️ Remove MapsViewer link

--- views/common/header.php
+++ views/common/header.php
@@ -50,7 +50,6 @@
             <a class="navbar-brand" href="./"><img src="./public/images/LostariaLogo2.png" height="50" width="50" /> </a>
             <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex flex-grow justify-content-end flex-grow p-2">
                 <a href="https://shop.lostaria.fr" target="_blank" class="btn btn-outline-light" style="margin-left: 10px;">Boutique</a>
-                <a href="https://maps.lostaria.fr" target="_blank" class="btn btn-outline-light" style="margin-left: 10px;">Voir les maps</a>
                 <a href="https://status.lostaria.fr" target="_blank" class="btn btn-outline-light" style="margin-left: 10px;">Etat des services</a>
             </ul>
         </div>
