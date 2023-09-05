d461343b4d655fe3cc62ec40898018afa3e91c4a
Erpriex <erpriex@gmail.com>
2023-09-05 18:59:15 +0200

    🚀 Home page

--- views/global/home.php
+++ views/global/home.php
@@ -1,31 +1,53 @@
 <div class="container" style="background-color: #FFF; border-radius: 10px; padding-top: 15px; padding-left: 15px; padding-right: 55px;">
 
-    <h4 style="margin: 20px">Membres de l'équipe :</h4>
-    <?php
+    <div style="margin: 20px;">
+        <h1>Bienvenue sur Lostaria !</h1>
+        <p style="margin-top: 20px;">D'origine une communauté de guildes du serveur Epicube, Lostaria est un serveur événementiel qui recréer les jeux qui nous ont rassemblés.</p>
+        <p>En développement constant, Lostaria réunit aujourd'hui une petite communauté autour de ses jeux dans une ambiance amicale et de détente.</p>
+    </div>
 
-    use entities\LostariaPlayer;
+    <div style="margin: 20px;">
+        <h2 style="margin-top: 50px;">Lostaria, c'est :</h2>
+        <div style="display: flex; justify-content: space-around; text-align: center; margin-top: 50px;">
+            <div>
+                <p style="font-size: 2rem; margin-bottom: 0px;">+15</p>
+                <p>jeux</p>
+            </div>
+            <div>
+                <p style="font-size: 2rem; margin-bottom: 0px;">+300</p>
+                <p>joueurs inscrits</p>
+            </div>
+        </div>
+    </div>
 
-    $i = 0;
-    foreach ($staffMembers as $p) {
-        if ($i >= 2) {
-            echo '</div>';
-            $i = 0;
-        }
-        if ($i == 0) {
-            echo '<div class="row">';
-        }
 
-        $competences = "";
-        foreach ($p[1] as $c) {
-            $competences = $competences . "<span style='margin-right: 3px' class=\"badge rounded-pill text-bg-primary text-light\">" . $c . "</span>";
-        }
+    <div style="margin-top: 50px;">
+        <h2 style="font-size: 1.5rem; margin: 20px;">Membres de l'équipe :</h2>
+        <?php
 
-        $redisPlayer = $this->redisModel->getOne("redisplayer:". $p[0]);
-        $playerObj = json_decode($redisPlayer, true);
-        $player = new LostariaPlayer($playerObj);
-        $staffName = $player->getName();
+        use entities\LostariaPlayer;
 
-        echo '
+        $i = 0;
+        foreach ($staffMembers as $p) {
+            if ($i >= 2) {
+                echo '</div>';
+                $i = 0;
+            }
+            if ($i == 0) {
+                echo '<div class="row">';
+            }
+
+            $competences = "";
+            foreach ($p[1] as $c) {
+                $competences = $competences . "<span style='margin-right: 3px' class=\"badge rounded-pill text-bg-primary text-light\">" . $c . "</span>";
+            }
+
+            $redisPlayer = $this->redisModel->getOne("redisplayer:". $p[0]);
+            $playerObj = json_decode($redisPlayer, true);
+            $player = new LostariaPlayer($playerObj);
+            $staffName = $player->getName();
+
+            echo '
                 <div style="margin-left: 20px; margin-bottom: 20px;" class="card card-hover">
                         <div class="card-body d-flex">
                             <div class="p-3">
@@ -40,12 +62,13 @@
                         </div>
                     </div>
             ';
-        $i++;
-    }
-    if ($i >= 2) {
-        echo '</div>';
-    }
-    ?>
+            $i++;
+        }
+        if ($i >= 2) {
+            echo '</div>';
+        }
+        ?>
+    </div>
 </div>
 
 <style>
