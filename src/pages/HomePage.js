78f3579e907243f78db83ca1cd916eca5969c4bc
Erpriex <erpriex@gmail.com>
2025-05-11 01:15:38 +0200

    🎉 Begin project

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import styles from "../styles/pages/Home.module.scss";
+
+const HomePage = () => {
+  return (
+    <section className={styles.container}>
+      <p>Hello world !</p>
+    </section>
+  );
+};
+
+export default HomePage;
